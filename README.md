f63956114705cb39a008c1b112592e0a330630ae
Irid3 <katrokk54@gmail.com>
2023-01-28 19:10:58 +0700

    adding README

---
+++ README.md
@@ -0,0 +1,84 @@
+# Install Module
+Install dulu requirmentnya
+```
+pip install -r requirements.txt
+```
+atau
+```
+pip3 install -r requirements.txt
+```
+
+# Running
+
+## Windows
+
+```
+Start-Process python -ArgumentList "main.py" -NoNewWindow -Wait
+```
+Program akan berhenti kalau terminal diclose.
+
+<br />
+
+---
+Kalau ingin program berjalan di background :
+---
+
+<br />
+
+```
+powershell -Command "Start-Process python -ArgumentList 'main.py' -NoNewWindow -RedirectStandardOutput out.txt -RedirectStandardError err.txt -RedirectStandardInput in.txt
+```
+
+## Linux
+
+<br />
+
+```
+nohup python main.py > output.log 2>&1 &
+```
+<br />
+
+
+# Check realtime output
+<br />
+
+## Windows
+```
+Get-Content -path out.txt -Wait
+```
+<br />
+
+## Linux
+```
+tail -f output.log
+```
+
+# Stop Program
+## Windows
+
+<br />
+
+```
+Stop-Process -Id (Get-Process -Name "python" | Select-Object -ExpandProperty Id)
+```
+
+<br />
+
+## Linux
+<br />
+cek dulu pid pythonnya berapa :
+
+<br />
+
+```
+ps aux | grep python
+```
+<br />
+
+kalau udah ketemu tinggal :
+
+<br />
+
+```
+tail -f output.log
+```
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import random,time,requests,socket,datetime
+import random,time,requests,socket,datetime,os
 from fake_useragent import UserAgent
 ua = UserAgent()
 
@@ -6,13 +6,12 @@ ua = UserAgent()
 def randomIp():
     classIp = ["a","b","c"]
     ip_standart = [i for i in range(1,254)]
-    # print("ip : ",random.choice(classIp))
     decision = random.choice(classIp)
-    a = 0
+    a = 0#Breakpoint
     if(decision == "c"):
         _1,_2,_3 = str(random.choice(ip_standart)),str(random.choice(ip_standart)),str(random.choice(ip_standart))
         endIp = "".join([_1,".",_2,".",_3,".",str(random.choice(ip_standart))])
-        if(endIp == None):
+        if(endIp == None):#cek jika return None
             randomIp()
         return endIp
     elif(decision == "b"):
@@ -40,51 +39,46 @@ def sendReq(mail):
     }
     res = requests.post(url,data = datas,headers=header)
     msg = res.json()
-    print(res)
     if 'msg' in msg:
         if msg['msg'] == 'S':
             print("Success")
         else:
             print("Failed")
-# for i in range(100):
-#     print(randomIp())
-# with open("1M_GMAIL_mail_no_pass.txt") as f:
-#     j = 0
-#     for i in f:
-#         j += 1
-#         sendReq(i)
-#         time.sleep(.5)
-#         if(j == 200000):
-#             break
 
-time_est = 0.5 * 200000
+
+time_est = 5 * 100000
 est_time = datetime.datetime.now() + datetime.timedelta(seconds=time_est)
 print(f"Estimasi program selesai: {est_time}")
 
 
 failed_data = []
 with open("1M_GMAIL_mail_no_pass.txt") as f:
+    lines = f.readlines()
     j = 0
-    for i in f:
+    for i in lines[300020:]:#loncat ke baris 300020
         j += 1
+        print("request ke ",j)
         while True:
             try:
                 socket.create_connection(("www.google.com", 80))
-                if i in failed_data:
-                    sendReq(i)
-                    failed_data.remove(i)  
+                if i.replace("\n","") in failed_data:
+                    sendReq(i.replace("\n",""))
+                    failed_data.remove(i.replace("\n",""))  
                 else:
-                    sendReq(i)
+                    sendReq(i.replace("\n",""))
                 break
             except OSError:
                 print("Tidak ada internet, mencoba lagi dalam 5 detik...")
-                failed_data.append(i)
+                failed_data.append(i.replace("\n",""))
                 time.sleep(5)
                 continue
             except Exception as e:
                 print(f"ada yang error: {e}")
-                failed_data.append(i)
+                failed_data.append(i.replace("\n",""))
                 break
-        time.sleep(.5)
-        if(j == 200000):
+        if j % 10 == 0:
+            os.system('cls')
+            print(f"Estimasi program selesai: {est_time}")
+        time.sleep(5)#delay 5 detik, kasian server countersidenya kalau pake delay milisecond
+        if(j == 50000):#stop kalau udah 50k request
             break
